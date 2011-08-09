93a22dc0535b0bc5009c68b53dbe6d593df5cd5c
da xu <mrfrinx@gmail.com>
2011-08-09 15:20:47 -0700

    update euler46

--- euler46.py
+++ euler46.py
@@ -1,12 +1,33 @@
-import IsPrime
+from IsPrime import IsPrime
+from math import sqrt
+
+primes = [2]
+
+def IsPerfectSquare(n):
+  x = sqrt(n)
+  x = int(x)
+  if x*x==n:
+    return True
+  else: return False
 
 def Euler46():
-  go=true
-  i=1
-  while go:
-    if (!IsPrime.IsPrime(i)):
+  i=3
+  while 1:
+    if IsPrime(i):
+      primes.append(i)  
+    else:
       # check conjecture, all primes + some 2*k^2 == i?
-      
+      found=False
+      for p in primes:
+        n = (i-p)/2
+        if IsPerfectSquare(n):
+          found=True
+          #print 'break for i: ',i,' p: ',p,' n: ',n
+          break
+      if found==False:
+        print 'found Goldman #: ',i
+        exit()
     i+=2
+    #if i%10000==1: print i
 
-print "hello"
+Euler46()
